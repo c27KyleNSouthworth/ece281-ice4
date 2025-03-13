9a0d0e735f40e887b7544a3cd9ebd05ed28086c5
c27KyleNSouthowrth <c27kyle.southworth@afacademy.af.edu>
2025-03-13 14:26:35 -0600

    initialCommit

--- src/stoplight_fsm.vhd
+++ src/stoplight_fsm.vhd
@@ -71,7 +71,7 @@ end stoplight_fsm;
 
 architecture stoplight_fsm_arch of stoplight_fsm is 
 	
-	-- create register signals with default state yellow (10)
+	-- create register signals with default state yellow (10)  
   
 begin
 	-- CONCURRENT STATEMENTS ----------------------------
